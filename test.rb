701f4bb6886cbaee7d6562ecbcb97a08bca44973
amacha2626 <amacha2626@gmail.com>
2020-07-27 20:56:27 +0900

    super

--- test.rb
+++ test.rb
@@ -25,3 +25,37 @@ end
 4.times do |i|
   p i
 end
+
+p "------------------------"
+
+class Test
+  def foo(int)
+    p int
+  end
+end
+
+test = Test.new
+test.foo(5)
+
+class Foo
+  def initialize(a)
+    @a = a
+  end
+  def method1
+    @a
+  end
+end
+  
+class FooExt < Foo
+  def initialize(a,b)
+    @b = b
+    super a
+  end
+  def method2(c)
+    @a + @b + c
+  end
+end
+
+fooExt = FooExt.new(3,4)
+p fooExt.method1
+p fooExt.method2(5)
