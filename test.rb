755c78aa24e333ff44b2eb60d7dee9ee255d64db
amacha2626 <amacha2626@gmail.com>
2020-07-30 15:42:45 +0900

    open mode of File class

--- test.rb
+++ test.rb
@@ -287,3 +287,9 @@ p a
 v1 = 1 - 1 == 0
 v2 = v1 || raise(RuntimeError)
 puts v2 && false
+
+File.open('testfile.txt', 'a+') do |f|
+  f.write("recode 1\n")
+  f.seek(0, IO::SEEK_SET)
+  f.write("recode 2\n")
+end
