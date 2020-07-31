e5a81f795fc0d47e1bd8cf980cd776a8295f687e
amacha2626 <amacha2626@gmail.com>
2020-07-31 12:27:28 +0900

    attr method

--- test.rb
+++ test.rb
@@ -531,3 +531,25 @@ end
  
 puts Baz::Bar
 puts Dog::Bar
+
+@v1 = 1
+class Qux3
+  @v2 = 2
+  def method3
+    puts @v1
+  end
+  def method4
+    puts @v2
+  end
+end
+
+Qux3.new.method3
+Qux3.new.method4
+
+class Qux4
+  attr_accessor :v3
+  def method1; v3; end
+end
+qux4 = Qux4.new
+# p qux4.v3 = 3
+p qux4.method1
