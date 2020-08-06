119bce0e02b67e61b90f6abfbc9814055ce10866
amacha2626 <amacha2626@gmail.com>
2020-08-07 00:11:00 +0900

    include and extend method of Class

--- gold.rb
+++ gold.rb
@@ -127,3 +127,18 @@ end
 p Bar.c_method2
 p Bar.c_method3
 p Bar.c_method4
+
+p "---------"
+
+module M4
+  def method1; 1; end
+end
+
+class C4
+  include M4
+  extend M4
+end
+
+p C4.method1
+c4 = C4.new
+p c4.method1
