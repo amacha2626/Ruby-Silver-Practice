1f5d7380b9e40151bf97d6e72081e248f81b2ff4
amacha2626 <amacha2626@gmail.com>
2020-08-06 18:19:41 +0900

    regexp $

--- gold.rb
+++ gold.rb
@@ -2,3 +2,11 @@ p /a\db/ === "a3b"
 p /a\db/ === "a3bd"
 p /a(bc)*d/ === "abcbcd"
 p /a(bc){2,4}d/ === "abcbcbcbcd"
+
+%r|(http://www(\.)(.*)/)| =~ "http://www.xyz.org/"
+p $1
+p $2
+p $3
+p $4
+
+p %r|(http://www(\.)(.*)/)| === "http://www.xyz./"
