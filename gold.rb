9b3053fd001966ddeef2dac5ef9b9f88cdeb4e26
amacha2626 <amacha2626@gmail.com>
2020-08-06 23:00:40 +0900

    ancestors method

--- gold.rb
+++ gold.rb
@@ -54,3 +54,24 @@ singleton_class = class << foo1
   self
 end
 p singleton_class
+
+
+module M1
+  def method1; "m1"; end
+end
+
+class C1
+  prepend M1
+  def method1; "c1"; end
+end
+
+class C2
+  include M1
+  def method1; "c2"; end
+end
+
+p C1.new.method1
+p C2.new.method1
+
+p C1.ancestors
+p C2.ancestors
