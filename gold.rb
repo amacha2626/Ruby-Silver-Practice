86a7f1c4c65267a437913e9b584ecd770e82cb58
amacha2626 <amacha2626@gmail.com>
2020-08-07 11:28:09 +0900

    Thread class

--- gold.rb
+++ gold.rb
@@ -176,3 +176,18 @@ f.call
 
 f = Proc.new{|i| puts i}
 3.times(&f)
+
+# t = Thread.new do
+#   p "start"
+#   sleep 5
+#   p "end"
+# end
+# p "wait"
+# t.join
+
+3.times do |i|
+  Thread.start(i) do |index|
+    p "thread-#{index} start"
+  end
+end
+# sleep 1
