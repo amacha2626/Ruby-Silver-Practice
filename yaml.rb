e9497a5f3a572935a2c47d6a2e0a4d316a5509fb
amacha2626 <amacha2626@gmail.com>
2020-08-09 23:15:57 +0900

    dum method of fortmat yaml

--- yaml.rb
+++ yaml.rb
@@ -12,3 +12,6 @@ DATA
 p YAML.load(yaml_data)
 p YAML.load_stream(yaml_data)
 YAML.load_stream(yaml_data){|yaml| p yaml.first}
+
+p YAML.dump YAML.load(yaml_data)
+p YAML.dump YAML.load_stream(yaml_data)
