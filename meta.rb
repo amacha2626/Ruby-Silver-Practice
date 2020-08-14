1d11d41645448a1e52a277f143fe8f93045dda20
amacha2626 <amacha2626@gmail.com>
2020-08-14 12:34:32 +0900

    flat scope

--- meta.rb
+++ meta.rb
@@ -68,3 +68,14 @@ end
 
 p cat
 p dog.class
+
+my_var = "成功"
+MyClass2 = Class.new do
+  puts "クラス定義の中は#{my_var}"
+
+  define_method :my_method do
+    "メソッド定義の中も#{my_var}"
+  end
+end
+
+puts MyClass2.new.my_method
