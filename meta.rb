04dbc763080e1b608b7e2d4412d032ebdbdedfbb
amacha2626 <amacha2626@gmail.com>
2020-08-20 11:19:40 +0900

    Module#alias_method

--- meta.rb
+++ meta.rb
@@ -269,3 +269,12 @@ obj.extend MyModule2
 
 p obj.my_method
 p obj.singleton_methods
+
+class MyClass
+  def my_method; "my_method()"; end
+  alias_method :m, :my_method
+end
+
+obj = MyClass.new
+p obj.my_method
+p obj.m
