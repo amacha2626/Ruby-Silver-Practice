5fb7cd298604cf14ffd21a45c4344f78cd9ca51e
amacha2626 <amacha2626@gmail.com>
2020-08-23 15:11:06 +0900

    step method of date library

--- date.rb
+++ date.rb
@@ -2,3 +2,9 @@ require 'date'
 
 p DateTime.new(2011, 7, 19) - DateTime.new(2010, 8, 27)
 p DateTime.new(2011, 7, 19) - DateTime.new(2010, 7, 19)
+
+start_from = Date.new(2017, 7, 1)
+end_to = Date.new(2017, 7, 31)
+start_from.step(end_to, 7) do |date|
+  p date.to_s
+end
