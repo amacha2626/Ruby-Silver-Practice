d29e64d610d6b852ea13244eed2c0fb9b20b58ef
amacha2626 <amacha2626@gmail.com>
2020-08-26 18:23:48 +0900

    argment

--- meta.rb
+++ meta.rb
@@ -433,3 +433,14 @@ end
 p "12345".hoge
 
 p ['a' ,'b'].map(&:upcase)
+
+def hoge(*args, &block)
+  block.call(args)
+end
+
+hoge(1,2,3,4) do |*args|
+  p args.length < 0 ? "hello" : args
+end
+hoge(1,2,3,4) do |args|
+  p args.length < 0 ? "hello" : args
+end
