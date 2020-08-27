fbf76ff7aeb9941d3f2ff655d50041867a898119
amacha2626 <amacha2626@gmail.com>
2020-08-27 17:46:36 +0900

    instance and module eval

--- meta.rb
+++ meta.rb
@@ -520,3 +520,22 @@ module M
     p CONST
   end
 end
+
+m = Module.new
+
+CONST = "Constant in Toplevel"
+
+_proc = Proc.new do
+  CONST = "Constant in Proc"
+end
+
+m.instance_eval(<<-EOS)
+  CONST = "Constant in Module instance"
+
+  def const
+    CONST
+  end
+EOS
+
+p m.module_eval(&_proc)
+p m.const
