f4d7e53900321621912598b59132a5a842651854
amacha2626 <amacha2626@gmail.com>
2020-08-27 17:35:16 +0900

    to_enum and enum_for method

--- meta.rb
+++ meta.rb
@@ -479,3 +479,18 @@ companies.each do |e|
 end
 
 p "a".succ
+
+class Array
+  def succ_each(step = 1)
+    return to_enum(__method__, step) unless block_given?
+
+    each do |int|
+      yield int + step
+    end
+  end
+end
+
+[97, 98, 99].succ_each.map {|int|
+  p int.chr
+}
+p [97, 98, 99].succ_each.map
